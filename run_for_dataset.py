5a3e8a178a7bb7fdec1b7dd59a50c6d3e895d7c1
mattiadurso <mattiadurso98@gmail.com>
2025-10-22 15:20:16 +0200

    .

---
+++ run_for_dataset.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+from vggt.wrapper import VGGTWrapper
+
+
+paths = {
+    "imc_local": {
+        "base_path": "/home/mattia/Desktop/Repos/wrapper_factory/benchmarks_2D/imc/data/phototourism",
+        "images_path": "set_100/images",
+        "output_path": "/home/mattia/Desktop/Repos/wrapper_factory/benchmarks_3D/results",
+    },
+    "imc_orochi": {
+        "base_path": "/data/mdurso/imc",
+        "images_path": "set_100/images",
+        "output_path": "/data/mdurso/imc/results",
+    },
+    "eth3d_local": {
+        "base_path": "/home/mattia/Desktop/Repos/wrapper_factory/benchmarks_3D/eth3d",
+        "images_path": "images_by_k",
+        "output_path": "/home/mattia/Desktop/Repos/wrapper_factory/benchmarks_3D/results",
+    },
+    "eth3d_orochi": {
+        "base_path": "/data/mdurso/eth3d",
+        "images_path": "images_by_k",
+        "output_path": "/data/mdurso/eth3d/results",
+    },
+}
+
+
+dataset = "imc_orochi"
+base_path = paths[dataset]["base_path"]
+images_path = paths[dataset]["images_path"]
+output_path = paths[dataset]["output_path"]
+scenes = sorted(os.listdir(f"{base_path}"))
+
+for scene in scenes:
+    os.system(
+        f"python scripts/demo_colmap.py \
+            --scene_dir {base_path}/{scene} \
+            --images_dir {base_path}/{scene}/{images_path} \
+            --out_dir {output_path}/map_anything/{dataset}/{scene} \
+    "
+    )
